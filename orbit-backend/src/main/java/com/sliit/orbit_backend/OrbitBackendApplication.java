b163f25e68cf7f79acae73b198f37d076b34a48a
Dayan Wishwanath <68376218+dayanwishwanathr@users.noreply.github.com>
2026-04-16 11:00:04 +0530

    create project foundation

---
+++ orbit-backend/src/main/java/com/sliit/orbit_backend/OrbitBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sliit.orbit_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrbitBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrbitBackendApplication.class, args);
+	}
+
+}
