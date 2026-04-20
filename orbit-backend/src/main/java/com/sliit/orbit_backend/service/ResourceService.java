928797476fd0ce9c98c079261ecc13da5702b601
Lakshitha200206 <ravindulakshitha624@gmail.com>
2026-04-21 03:22:35 +0530

    feat(service): create ResourceService read methods

---
+++ orbit-backend/src/main/java/com/sliit/orbit_backend/service/ResourceService.java
@@ -0,0 +1,23 @@
+package com.sliit.orbit_backend.service;
+
+import com.sliit.orbit_backend.model.Resource;
+import com.sliit.orbit_backend.repository.ResourceRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ResourceService {
+
+    private final ResourceRepository resourceRepository;
+
+    public List<Resource> getAllResources() {
+        return resourceRepository.findAll();
+    }
+
+    public Resource getResourceById(String id) {
+        return resourceRepository.findById(id).orElseThrow(() -> new RuntimeException("Resource not found"));
+    }
+}
